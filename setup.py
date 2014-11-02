539bed9c7438eeae35aca9726aa30895c8d3b938
Frazer McLean <frazer@frazermclean.co.uk>
2014-11-02 03:14:29 +0100

    Add metadata for PyPI

--- setup.py
+++ setup.py
@@ -16,12 +16,17 @@ class PyTest(Command):
         raise SystemExit(errno)
 
 
-setup(name='orbit_data_messages',
-      version='0.1.0',
-      description='Create valid ASCII OPM, OMM, and OEM files.',
-      long_description=open('README.rst').read(),
-      author='Frazer McLean',
-      author_email='frazer@frazermclean.co.uk',
-      url='https://github.com/RazerM/orbit-data-messages',
-      packages=['orbitdatamessages'],
-      cmdclass = {'test': PyTest})
+setup(
+    name='orbit_data_messages',
+    version='0.1.0',
+    description='Create valid ASCII OPM, OMM, and OEM files.',
+    long_description=open('README').read(),
+    author='Frazer McLean',
+    author_email='frazer@frazermclean.co.uk',
+    url='https://github.com/RazerM/orbit-data-messages',
+    packages=['orbitdatamessages'],
+    cmdclass = {'test': PyTest},
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+    ]
+)
