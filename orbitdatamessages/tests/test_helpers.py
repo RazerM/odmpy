723d0cbf2e4dd59b96bb9d1e34e763149de5836f
Frazer McLean <frazer@frazermclean.co.uk>
2014-11-05 00:33:46 +0100

    Start tests of auxiliary functions

---
+++ orbitdatamessages/tests/test_helpers.py
@@ -0,0 +1,17 @@
+import unittest
+
+import orbitdatamessages.orbital_parameter_message as opm
+
+
+class TestHelpers(unittest.TestCase):
+    def test_prefix(self):
+        a = ['A', 'B', 'C', '']
+        b = ['AA', 'AB', 'AC', 'A']
+        for i, x in enumerate(opm.prefix('A', a)):
+            self.assertEqual(b[i], x)
+
+    def test_suffix(self):
+        a = ['A', 'B', 'C', '']
+        b = ['AA', 'BA', 'CA', 'A']
+        for i, x in enumerate(opm.suffix('A', a)):
+            self.assertEqual(b[i], x)
