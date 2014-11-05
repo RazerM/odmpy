362367e567f2d543fd057d9fd7ca45928745654e
Frazer McLean <frazer@frazermclean.co.uk>
2014-11-05 23:57:42 +0100

    Preparing release 0.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class PyTest(Command):
 
 setup(
     name='orbit_data_messages',
-    version='0.1.0',
+    version='0.1.1',
     description='Create valid ASCII OPM, OMM, and OEM files.',
     long_description=open('README').read(),
     author='Frazer McLean',
