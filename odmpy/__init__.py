561662894cb2b6aa47f7b453b922104545f49984
Frazer McLean <frazer@frazermclean.co.uk>
2014-12-07 17:13:48 +0100

    Preparing release 0.2.4

--- odmpy/__init__.py
+++ odmpy/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 __author__ = 'Frazer McLean <frazer@frazermclean.co.uk>'
-__version__ = '0.2.4.dev0'
+__version__ = '0.2.4'
 __license__ = 'MIT'
 __description__ = 'Create valid ASCII OPM, OMM, and OEM files.'
 
